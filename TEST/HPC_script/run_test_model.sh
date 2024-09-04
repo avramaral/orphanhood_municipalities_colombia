1de94c495bedb984b1ecb924214c4a099170a1f0
avramaral <avramaral@gmail.com>
2024-09-04 15:46:25 +0100

    update simple example

--- TEST/HPC_script/run_test_model.sh
+++ TEST/HPC_script/run_test_model.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #PBS -l walltime=23:59:00
-#PBS -l select=1:ncpus=4:ompthreads=4:mem=1gb
+#PBS -l select=1:ncpus=4:ompthreads=4:mem=4gb
 
 REPO_PATH="/rds/general/user/aribeir2/home/orphanhood"
 
 module load anaconda3/personal
 source activate STANEnv
 
-Rscript TEST/test_model.R \
-  -i "$REPO_PATH"
+cd $REPO_PATH
+Rscript TEST/test_model.R
