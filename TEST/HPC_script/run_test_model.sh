b6285421772723be1183371084796218564b71a5
avramaral <avramaral@gmail.com>
2024-09-04 15:40:22 +0100

    update simple model

--- TEST/HPC_script/run_test_model.sh
+++ TEST/HPC_script/run_test_model.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-REPO_PATH="/rds/general/user/aribeir2/home/orphanhood"
-
-#PBS -l walltime=00:10:00
+#PBS -l walltime=23:59:00
 #PBS -l select=1:ncpus=4:ompthreads=4:mem=1gb
+
+REPO_PATH="/rds/general/user/aribeir2/home/orphanhood"
 
 module load anaconda3/personal
 source activate STANEnv
