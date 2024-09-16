6d7baf8462f15a1b61bbd77d8bc2f646520973cd
avramaral <avramaral@gmail.com>
2024-09-16 19:23:39 +0100

    update national model

---
+++ HPC_script/run_national_mortality_bias_model.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#PBS -l walltime=48:00:00
+#PBS -l select=1:ncpus=8:ompthreads=8:mem=40gb
+
+REPO_PATH="/rds/general/user/aribeir2/home/orphanhood"
+
+module load anaconda3/personal
+source activate STANEnv
+
+cd $REPO_PATH
+Rscript mortality_national.R
