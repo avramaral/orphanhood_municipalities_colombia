94671ccc812c954ce4af556ae53028dc1013830b
avramaral <avramaral@gmail.com>
2024-09-18 18:03:25 +0100

    update sh scripts

--- HPC_script/run_mortality_bias_model.sh
+++ HPC_script/run_mortality_bias_model.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #PBS -l walltime=23:59:00
-#PBS -l select=1:ncpus=4:ompthreads=4:mem=80gb
+#PBS -l select=1:ncpus=4:ompthreads=4:mem=100gb
 
 REPO_PATH="/rds/general/user/aribeir2/home/orphanhood"
 
